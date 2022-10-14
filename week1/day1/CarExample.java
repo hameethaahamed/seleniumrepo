08294d9363718c4537241c4d020c0d22ce8935d8
hameethaahamed <hameethaahamed@gmail.com>
2022-10-15 00:52:23 +0400

    Add files via upload

---
+++ week1/day1/CarExample.java
@@ -0,0 +1,37 @@
+package week1.day1;
+
+public class CarExample {
+/*	
+	public static void main(String[] args)
+	{
+
+	String make = "Mitsubishi";
+	float number1 = 23425.6426F;
+	double number2 = 944458979.2589589487;
+	
+	char initiaj = 'H';
+	boolean registered = true;
+	int modelno = '5';
+	public int getRegistrationNumber() {
+		
+	}
+*/	
+	public Void printCarName() {
+		System.out.println("Mitsubhisi Krypto");
+		return null;
+	}
+	public Boolean isCarPuncture() {
+		return true;
+		
+	}
+	public String getColourofThecar() {
+		String colour ="Grey";
+		return colour;
+	}
+	public int getRegNumber() {
+		int regnumber = 4589;
+		return regnumber;
+	}
+	
+	
+}
---
+++ week1/day1/Factorial.java
@@ -0,0 +1,16 @@
+ package week1.day1;
+
+public class Factorial {
+public static void main (String args[]) {
+	
+	int b=1;
+	for(int i = 1; i <= 5; i++) {
+		b =i*b;
+		System.out.println("Print b:" + b);
+		
+	}
+	System.out.println("Factorial of 5 is :" + b);
+	}
+	
+}
+
